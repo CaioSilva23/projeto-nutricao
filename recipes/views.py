3b771625c0647d5b9982d99c1a1f6dfda7d36c7f
Caio Silva <caioceac23@hotmail.com>
2022-07-03 18:13:07 -0300

    removido contato

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -8,10 +7,3 @@ def home(request):
         'name':'Caio Silva'
     }
     )
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
